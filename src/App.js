d8e12738ada5dbd80ca5040665e0fdd599dcb2f1
Maryam M.Alizadeh <32436512+maryamalizadeh91@users.noreply.github.com>
2023-07-13 13:49:01 +0330

    Added Background Image

--- src/App.js
+++ src/App.js
@@ -5,25 +5,27 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <h1>Weather App</h1>
-        <Weather defaultCity="Tabriz" />
-        <footer>
-          This project is created by Maryam{" "}
-          <a
-            href="https://www.linkedin.com/in/maryamalizadeh91/"
-            target="_blank"
-            rel="noreferrer">
-            M.Alizadeh
-          </a>{" "}
-          and is{" "}
-          <a
-            href="https://github.com/maryamalizadeh91/react-weather-week5"
-            target="_blank"
-            rel="noreferrer">
-            open-sourced on GitHub
-          </a>
-        </footer>
+      <div className="app-container align-middle">
+        <div className="container">
+          <h1>Weather App</h1>
+          <Weather defaultCity="Tabriz" />
+          <footer>
+            This project is created by Maryam{" "}
+            <a
+              href="https://www.linkedin.com/in/maryamalizadeh91/"
+              target="_blank"
+              rel="noreferrer">
+              M.Alizadeh
+            </a>{" "}
+            and is{" "}
+            <a
+              href="https://github.com/maryamalizadeh91/react-weather-week5"
+              target="_blank"
+              rel="noreferrer">
+              open-sourced on GitHub
+            </a>
+          </footer>
+        </div>
       </div>
     </div>
   );
